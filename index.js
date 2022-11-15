b0dd6f86416de9f0e43510ef6cd702f40af71733
Gustavo <gustavommello03@gmail.com>
2022-11-15 10:15:45 +0100

    node initialized

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const app = express()
+const port = 3000;
+
+
+app.use(express.static("public"));
+
+app.set('views', './views');
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname +  "/public/index.html")
+})
+
+app.get("/qrcode", (req, res) => {
+    res.sendFile(__dirname +  "/views/qrcode.html")
+})
+
+app.listen(port, () => {
+    console.log('Listening in port: ' + port)
+})
+
