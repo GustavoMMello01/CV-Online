1337c38ea4ad60bf01ef341c15699574e001bc4a
Gustavo <gustavommello03@gmail.com>
2022-11-18 11:24:25 +0100

    pdf started and night mode style fixed

--- public/js/index.js
+++ public/js/index.js
@@ -4,4 +4,25 @@ window.onload = function () {
     night_icon.onclick = function(){
         document.body.classList.toggle("dark-theme") 
     }
+
+    let areaCv = document.getElementById("area-cv")
+    let resumeButton = document.getElementById("export-pdf")
+
+    let opt = {
+        margin:       0,
+        filename:     'Gustavo_Mello_CV.pdf',
+        image:        { type: 'jpeg', quality: 0.98 },
+        html2canvas:  { scale: 4 },
+        jsPDF:        { unit: 'in', format: 'a4', orientation: 'portrait' }
+      };
+
+    function generateResume(){
+        html2pdf(areaCv, opt)
+    }
+
+    resumeButton.addEventListener('click', ()=> {
+        scaleCv()
+        generateResume()
+    })
+
 }
